338fa6f735f3d629c6d16dc82384a14036b666c2
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-04-30 23:34:52 +0000

    Removed trial/error print statements for debugging

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,18 +7,9 @@ import requests
 
 if __name__ == "__main__":
     my_user_url = 'https://jsonplaceholder.typicode.com/users'
-#    todo = 'https://jsonplaceholder.typicode.com/todos/'
-#    todo = 'https://jsonplaceholder.typicode.com/todos?userId='
     employee_response = requests.get(my_user_url)
     employee_data = employee_response.json()
-#    print(len(employee_data))
-#    employee_data = dict(employee_data)
-#    name = employee_data.get('username')
-#    print(todo_data)
     final_dict = {}
-#    for l in employee_data:
-
-#    for g in range(1, len(employee_data) + 1):
     my_dict = {}
     for g in employee_data:
         todo = 'https://jsonplaceholder.typicode.com/todos?userId='
@@ -38,61 +29,3 @@ if __name__ == "__main__":
     with open(my_file, 'w') as f:
         json_obj = json.dumps(final_dict)
         f.write(json_obj)
-#            for k, v in i.items():
-#                if k != 'userId' and k != 'id':
-#                    my_dict[k] = v
-#            the_list.append(my_dict)
-#        final_dict[g] = the_list
-#        print(final_dict)
-
-#            my_dict = i
-#            the_list.append(my_dict)
-#        final_dict[g] = the_list
-#        print(final_dict)
-#            my_dict = {}
-#            for j in employee_data:
-#                val = j['id']
-#                if val == i.get('userId'):
-#                    my_dict['task'] = i.get('title')
-#                    my_dict['completed'] = i.get('completed')
-#                    the_list.append(my_dict)
-#    print(the_list)
-#    total_dict = {}
-#    linds_dict = {}
-#    new_dict = {}
-#    for a in todo_data:
-#        print(a.get('userId'))
-#        final_list = []
-#        for b in employee_data:
-#            if a.get('userId') == b.get('id'):
-#                new_dict['task'] = a.get('title')
-#                new_dict['username'] = b.get('username')
-#                new_dict['completed'] = a.get('completed')
-#        final_list.append(new_dict)
-#    count = 0
-#    for index in the_list:
-#        for beeb in index:
-#            print(index[beeb])
-#        print(count)
-#        count += 1
-#    print(the_list)
-#                    the_list.append(my_dict)
-#        print(l['id'])
-#    for me in the_list:
-#        print(me)
-#        final_dict[l['id']] = the_list
-#    print(final_dict)
-#    print()
-#    print(final_dict)
-#    print(i.get('userId'))
-#        final_dict[i.get('userId')] = the_list
-#    print(final_dict)
-#    print()
-#        print(i)
-#        print()
-#            my_dict['username'] = j.get('username')
-#            the_list.append(my_dict)
-#        print(the_list)
-#        print(the_list)
-#        final_dict[val] = the_list
-#        print(final_dict)
