af77f86822cafaec2e61fc379e088b7682828008
Lindsey Hemenez <lindseyhemenez@gmail.com>
2018-05-06 10:17:40 -0700

    Fixed pep8 style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/' + subreddit + '/about/.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+    UA_string = 'Mozilla/5.0 (Windows NT 6.1) '
+    UA_string += 'AppleWebKit/537.36 (KHTML, like Gecko) '
+    UA_string += 'Chrome/41.0.2228.0 Safari/537.36'
+    headers = {'User-Agent': UA_string}
     response = requests.get(url, headers=headers)
-    if response.status_code is not '200':
+    if response.status_code != 200:
         return 0
     json_data = response.json()
     data_dict = json_data.get('data')
